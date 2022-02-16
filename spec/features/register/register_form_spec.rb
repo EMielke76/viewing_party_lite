6961110299527403f4b14219a0e31608af9e7446
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-16 14:34:44 -0700

    Create test for happy path user regitration

--- spec/features/register/register_form_spec.rb
+++ spec/features/register/register_form_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Registration form page' do
+
+  before :each do
+    visit register_path
+  end
+
   it 'displays a form to register a new user' do
+
+    expect(page).to have_field(:name)
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+    expect(page).to have_field(:password_confirmation)
+    expect(page).to have_button("Create New User")
   end
 
   context 'happy path' do
     it 'takes the user to its dashboard on successful registration' do
+      fill_in :name, with: 'Bob Barker'
+      fill_in :email, with: 'BobBarker@ThePriceIsWrong.net'
+      fill_in :password, with: 'HappysABitch'
+      fill_in :password_confirmation, with: 'HappysABitch'
+
+      click_on("Create New User")
+      expect(current_path).to eq("/users/#{User.last.id}/dashboard")
     end
   end
 
@@ -18,5 +36,5 @@ RSpec.describe 'Registration form page' do
 
     it 'returns an error if passwords do not match' do
     end
-  end 
+  end
 end
