f1b8f7878d5780f4dde65f8cae210c29c6c47840
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-16 14:08:47 -0700

    User validating Bcrypt necessaries

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :parties, through: :attendees
 
   validates :email, uniqueness: true, presence: true
+  validates :password_digest, presence: true
+  has_secure_password
 end
