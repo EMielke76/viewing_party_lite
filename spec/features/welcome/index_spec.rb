3f16d92c41234ae71472dafbd5d40ecd67e8850a
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-17 23:51:11 -0700

    Create test for visitor visability on welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,39 +1,36 @@
 require 'rails_helper'
 
 describe 'welcome page' do
-  before do
-    @user1 = User.create(name:'Devin', email:'devin@faker.net', password: '123fake', password_confirmation: '123fake')
-    @user2 = User.create(name:'Eric', email:'eric@faker.net', password: '123fake', password_confirmation: '123fake')
-    visit root_path
-  end
 
   describe 'display' do
     it 'title of application' do
+      visit root_path
       expect(page).to have_content("Viewing Party")
     end
 
     it 'button to create a new user' do
+      visit root_path
       click_button 'Create a New User'
       expect(current_path).to eq('/register')
     end
 
     it 'list of existing users which link to the users dashboard' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
-      click_on "#{@user1.email}'s Dashboard"
-      expect(current_path).to eq(dashboard_path)
+      user = User.create(name:'Devin', email:'devin@faker.net', password: '123fake', password_confirmation: '123fake')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit root_path
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user2)
-      click_on "#{@user2.email}'s Dashboard"
+      click_on "#{user.email}'s Dashboard"
       expect(current_path).to eq(dashboard_path)
     end
 
     it 'header link to go back to landing page' do
+      user1 = User.create(name:'Devin', email:'devin@faker.net', password: '123fake', password_confirmation: '123fake')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+      visit root_path
       click_on 'Home'
       expect(current_path).to eq('/')
       
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
-      click_on "#{@user1.email}'s Dashboard"
+      click_on "#{user1.email}'s Dashboard"
       expect(current_path).to eq(dashboard_path)
 
       click_on 'Home'
@@ -41,9 +38,19 @@ describe 'welcome page' do
     end
 
     it 'has a link to log in' do
+      visit root_path
       expect(page).to have_button("Log In")
       click_on("Log In")
       expect(current_path).to eq('/login')
+    end
+  end
+
+  describe 'visitor/non-registered users' do
+    it 'does not display the a list of existing users' do
+      visit root_path
+
+      expect(page).to_not have_content("Existing Users")
+      expect(page).to have_content("Log in or register to join the party!")
     end
   end
 end
